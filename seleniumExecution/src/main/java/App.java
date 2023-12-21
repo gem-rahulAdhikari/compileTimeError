36693a15257d9be3805848576b46a64a84a300ad
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-12-21 15:43:39 +0530

    Update App.java

--- seleniumExecution/src/main/java/App.java
+++ seleniumExecution/src/main/java/App.java
@@ -24,7 +24,7 @@ driver.get("https://www.google.com/");
      WebElement searchInput = driver.findElement(By.xpath("//textarea"));
     searchInput.sendKeys("selenium");
     searchInput.sendKeys(Keys.RETURN);
-     WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"))
+     WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
      String fetchedTitle=title.getText();
     System.out.println(fetchedTitle+" start2");      
     
