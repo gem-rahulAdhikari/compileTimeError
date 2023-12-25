78cda23788f849e2459d33e2ec3487def788c976
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-12-25 10:16:05 +0530

    Update YAML and Java files

--- seleniumExecution/src/main/java/App.java
+++ seleniumExecution/src/main/java/App.java
@@ -4,9 +4,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-
 
 import java.lang.*;
 import java.util.*;
@@ -14,56 +11,24 @@ import java.awt.*;
 import java.math.*;
 import java.time.*;
 
-public class App extends driverConfig{
-    static String reportName="Report_4a5d2e5676cf34734a533fe17b61b38dba979b836a374798302934e7b32ad777_0";
+public class App extends driverConfig {
+    static String reportName="Report_1703479560_0";
 
     @Test
+    public void demo() {
+        driver.get("https://www.google.com/");
+        WebElement searchInput = driver.findElement(By.xpath("//textarea"));
+        searchInput.sendKeys("selenium");
+        searchInput.sendKeys(Keys.RETURN);
+        WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
+        String fetchedTitle = title.getText();
+        System.out.println(fetchedTitle + " start2");
+        if ("Selenium TEST".equals(fetchedTitle)) {
+            extentTest.log(Status.PASS, "text matched successfully.", captureScreenshot());
+        } else {
+            extentTest.log(Status.FAIL, "Failed to match text.", captureScreenshot());
+        }
 
-public void demo(){
-driver.get("https://www.google.com/");
-     WebElement searchInput = driver.findElement(By.xpath("//textarea"));
-    searchInput.sendKeys("selenium");
-    searchInput.sendKeys(Keys.RETURN);
-     WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"))
-     String fetchedTitle=title.getText();
-    System.out.println(fetchedTitle+" start2");      
-    
-    if("Selenium".equals(fetchedTitle))
-     {
-         extentTest.log(Status.PASS,"text matched successfully.",captureScreenshot());
-     }
-    else
-     {
-         extentTest.log(Status.FAIL,"Failed to match text.",captureScreenshot());
-     }
- 
-     extentTest.log(Status.PASS,driver.getCurrentUrl(),captureScreenshot());
- 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        extentTest.log(Status.PASS, driver.getCurrentUrl(), captureScreenshot());
+    }
 }
